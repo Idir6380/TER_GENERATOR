7343791d704f1cf49fe83272ac3fb40f48e88f01
vanessaguerrier <vanessa.guerrier@etu.univ-amu.fr>
2026-02-11 17:33:46 +0100

    change

--- Nessa/preparation_data_train.py
+++ Nessa/preparation_data_train.py
@@ -1,6 +1,8 @@
 import re 
 from transformers import AutoTokenizer
-
+import requests
+from io import BytesIO
+from PyPDF2 import PdfReader
 
 def decomposition_en_phrase(text):
     text = re.sub(r'\s*,\s*', ' , ', text)
@@ -23,10 +25,10 @@ def extraire_nom_balise(tag):
     return re.sub(r'[</>]', '', tag)
 
 
-def labeliser(list_phrase):
+def labeliser(list_paragraphe):
     features = []
     labels = []
-    for phrase in list_phrase:
+    for phrase in list_paragraphe:
         fe ,la = [],[]
         i = 0
         n = len(phrase)
@@ -57,3 +59,82 @@ def labeliser(list_phrase):
         features.append(fe)
         labels.append(la)
     return features, labels
+
+
+def read_file_train(namefile):
+    text=""
+    with open(namefile, "r", encoding="utf-8") as file:
+        for line in file:
+            if line is not None :
+                text += line+" " 
+    return text
+
+
+def read_all(lits_file_name):
+    fe,la= [],[]
+    for name in lits_file_name:
+        text= read_file_train(name)
+        text= decomposition_en_list_mot(text)
+        features, labels= labeliser(text)
+        fe+=features
+        la+= labels
+    return fe,la
+
+def read_pdf_from_url(url):
+    headers = {
+        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) "
+                      "AppleWebKit/537.36 (KHTML, like Gecko) "
+                      "Chrome/112.0.0.0 Safari/537.36"
+    }
+    
+    response = requests.get(url, headers=headers)
+    if response.status_code != 200:
+        raise Exception(f"Erreur lors du téléchargement : {response.status_code}")
+    pdf_file = BytesIO(response.content)
+    reader = PdfReader(pdf_file)
+    text = ""
+    for page in reader.pages:
+        page_text = page.extract_text()
+        if page_text:
+            text += page_text + "\n"
+    return text
+
+
+def read_train_all(lits_file_name):
+    fe,la= [],[]
+    for name in lits_file_name:
+        text= read_file_train(name)
+        text= decomposition_en_list_mot(text)
+        features, labels= labeliser(text)
+        fe+=features
+        la+= labels
+    return fe,la
+
+def remove_references(text):
+    # Cherche le début après 'Abstract'
+    abstract_keywords = ["Abstract", "ABSTRACT"]
+    start_idx = 0
+    for kw in abstract_keywords:
+        idx = text.find(kw)
+        if idx != -1:
+            start_idx = idx + len(kw)
+            break  # on prend le premier trouvé
+
+    # Cherche la fin avant 'References' ou 'Bibliography'
+    reference_keywords = ["References", "REFERENCES", "Bibliography", "BIBLIOGRAPHY"]
+    end_idx = len(text)
+    for kw in reference_keywords:
+        idx = text.find(kw)
+        if idx != -1:
+            end_idx = idx
+            break
+    main_text = text[start_idx:end_idx].strip()
+    return main_text
+
+def read_file_test(url):
+    fe= []
+    text = read_pdf_from_url(url)
+    text = remove_references(text)
+    text= decomposition_en_list_mot(text)
+    fe+= text
+    return fe
