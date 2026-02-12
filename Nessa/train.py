bf18e23119f1b7acd94865517e69d6e9d3dab318
vanessaguerrier <vanessa.guerrier@etu.univ-amu.fr>
2026-02-12 17:47:34 +0100

    fichier train

--- Nessa/train.py
+++ Nessa/train.py
@@ -1,5 +1,15 @@
-from transformers import  AutoModelForTokenClassification
+from transformers import  AutoModelForTokenClassification,AutoTokenizer
+from preparation_data_train import data
+import json
 
+model_name="bert-base-cased"
+file_name = '/Users/vanessaguerrier/Downloads/M2_TER/data/all_articles.json'
+tokenizer = AutoTokenizer.from_pretrained(model_name)
 
+dataloader_train,dataloader_eval,fe_test,la_test,vocab,inv_vocab=data(file_name,tokenizer,300,batch_size=32)
+print(len(dataloader_eval),len(dataloader_train))
 def fit(vocab_t,inv_vocab_t):
-    model= AutoModelForTokenClassification.from_pretrained("bert-base-cased",num_labels=len(vocab_t),id2label=inv_vocab_t,label2id=vocab_t)
+    model= AutoModelForTokenClassification.from_pretrained(num_labels=len(vocab_t),id2label=inv_vocab_t,label2id=vocab_t)
+    
+    
+
