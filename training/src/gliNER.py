b8d8a9089135642314c942ddf8078c73b392a1a0
vanessaguerrier <vanessa.guerrier@etu.univ-amu.fr>
2026-03-09 18:26:58 +0100

    fd

--- training/src/gliNER.py
+++ training/src/gliNER.py
@@ -1,10 +1,18 @@
 from preparation_data_train import *
 from gliner import GLiNER
+from prediction_greenmir import deduplicate_information
 
 def chunk_text(text, max_chars=800):
     chunks = []
-    for i in range(0, len(text), max_chars):
-        chunks.append(text[i:i+max_chars])
+    current = ""
+    for p in text:
+        if len(current) + len(p) <= max_chars:
+            current += " " + p
+        else:
+            chunks.append(current.strip())
+            current = p
+    if current:
+        chunks.append(current.strip())
     return chunks
 
 def lecture_and_chunks(file, max_chars=800):
@@ -22,9 +30,36 @@ def prediction(file,model,labels, max_chars=800):
         all_entities.extend(ents)
     return all_entities 
 
-def affichage(all_entities):
+def affichage(all_entities,id):
+    dic = {
+            "article_id": id+1,
+            "information": {
+                "model_name": [],
+                "parameter_count": [],
+                "gpu_count": [],
+                "hardware": [],
+                "training_duration": [],
+                "country": [],
+                "year": []
+            }
+        }
     for e in all_entities:
-        print(f'{e["text"]} -> {e["label"]}')  
+        if e["label"] == "year":
+            dic["information"]["year"].append( e["text"])
+        elif e["label"] == "country":
+            dic["information"]["country"].append( e["text"])
+        elif e["label"] == "hardware":
+            dic["information"]["hardware"].append( e["text"])
+        elif e["label"] == "training duration":
+            dic["information"]["training_duration"].append( e["text"])
+        elif e["label"] == "gpu count":
+            dic["information"]["gpu_count"].append( e["text"])
+        elif e["label"] == "model name":
+            dic["information"]["model_name"].append( e["text"])
+        elif e["label"] == "parameter count":
+            dic["information"]["parameter_count"].append( e["text"])
+    dic= deduplicate_information(dic)
+    return dic
 
 
 
@@ -34,9 +69,12 @@ if __name__ == "__main__":
     labels= [
     "year", "gpu count", "training duration",
     "parameter count", "country", "hardware", "model name",]
-    
+    valeur =[]
     for i in range(113):
         file= f"data/GreenMIR/text_xml_nettoyer/article_{i+1}.txt"
         all_entities= prediction(file,model,labels, max_chars=100)
-        affichage(all_entities)
-        
+        dic = affichage(all_entities,i)
+        print(dic)
+        valeur.append(dic)
+    with open( "/Users/vanessaguerrier/Downloads/projet_TER_M2/data/GreenMIR/greenmir_pred_2.json", 'w', encoding='utf-8') as fichier:
+        json.dump(valeur, fichier, ensure_ascii=False, indent=4)
